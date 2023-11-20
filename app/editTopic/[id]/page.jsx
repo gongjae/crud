b52d1b4d2c7a56ea8ad23dbcb5c0e6cd14a39035
gongjae <rudddong@gmail.com>
2023-11-20 16:25:31 +0900

    1st

---
+++ app/editTopic/[id]/page.jsx
@@ -0,0 +1,9 @@
+import EditTopicForm from '@/components/EditTopicForm'
+import React from 'react'
+export default function EditTopic() {
+  return (
+    <div>
+      <EditTopicForm />
+    </div>
+  )
+}
