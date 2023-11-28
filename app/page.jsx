c039dd5a0df168d5a442bc7ef868b1aa55876370
gongjae <rudddong@gmail.com>
2023-11-28 11:46:18 +0900

    53th

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ export default function Home() {
             <h1 className="text-3xl font-bold">WebDev Topics</h1> 
       <p className="mb-4">MongoDB CRUD Example</p>
       <p className="mb-2">Next-auth 인증</p>
+      <TopicsList />
     </div>
   )
 }
