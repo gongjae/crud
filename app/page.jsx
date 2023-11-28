137edca99323197174111740d6a226c9f0b4d680
gongjae <rudddong@gmail.com>
2023-11-28 11:22:55 +0900

    3rd

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import TopicsList from '@/components/TopicsList'
 export default function Home() {
   return (
-    <>
+    <div>
             <h1 className="text-3xl font-bold">WebDev Topics</h1> 
       <p className="mb-4">MongoDB CRUD Example</p>
-    </>
+      <p className="mb-2">Next-auth 인증</p>
+      <TopicsList />
+    </div>
   )
 }
