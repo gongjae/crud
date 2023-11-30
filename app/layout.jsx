6c3e8c0f2ffd6403e8476199783346b063a2b507
gongjae <rudddong@gmail.com>
2023-11-30 21:04:58 +0900

    4th

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
-import { NextAuthProvider } from '@/components/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,12 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextAuthProvider>
-          <div className="max-w-5xl mx-auto p-4">
-            <Navbar />
-            <div className="mt-8">{children}</div>
-          </div>
-        </NextAuthProvider>
+        <div className="max-w-5xl mx-auto p-4">
+          <Navbar />
+          <div className="mt-8">{children}</div>
+        </div>
       </body>
     </html>
   )
