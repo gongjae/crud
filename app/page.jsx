502db1536fbfa51d1efa036c613024939bafc3fc
gongjae <rudddong@gmail.com>
2023-11-30 14:05:34 +0900

    4th

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,6 @@ export default async function Home() {
     <>
       <h1 className="text-3xl font-bold">WebDev Topics</h1>
       <p className="mb-4">MongoDB CRUD Example</p>
-      <TopicsList />
     </>
   )
 }
