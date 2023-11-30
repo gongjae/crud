c4decbb223ffb444a75be55196be2f1ecb9bdf73
gongjae <rudddong@gmail.com>
2023-11-30 20:02:26 +0900

    4th

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['lh3.googleusercontent.com'], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
